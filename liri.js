c907e0645ead5ff15f2902faf10fc3011fa6c63d
Dan Barthell <dbarthell@Dans-MacBook-Pro.local>
2018-05-13 11:14:48 -0500

    spotify api working

--- liri.js
+++ liri.js
@@ -2,12 +2,12 @@
 require("dotenv").config();
 
 var keys = require("keys.js");
-// var spotify = new Spotify(keys.spotify);
 var first = process.argv[2];
+var second = process.argv[3];
 
 // twitter API
 var Twitter = require('twitter');
-var client = new Twitter(keys.twitter);
+// var client = new Twitter(keys.twitter);
 
 
 var client = new Twitter({
@@ -23,5 +23,24 @@ if (first === "my-tweets") {
         if (!error) {
             console.log(tweets);
         }
+    });
+}
+
+// spotify API
+var Spotify = require('node-spotify-api');
+// var spotify = new Spotify(keys.spotify);
+
+var spotify = new Spotify({
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+});
+
+if (first === "spotify-this-song") {
+    spotify.search({ type: 'track', query: second }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        console.log(data);
     });
 }
